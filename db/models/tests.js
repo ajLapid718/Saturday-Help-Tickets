231f5273ee2412fee020f081cf20a4ef37dc7abc
Allan James Lapid <ajLapid718@gmail.com>
2018-05-02 16:03:50 -0400

    include subject and grade attributes in test table

--- db/models/tests.js
+++ db/models/tests.js
@@ -3,6 +3,17 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 const Student = require('./students');
 
-const Test = db.define();
+const Test = db.define('test', {
+  subject: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+
+  grade: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
+
+});
 
 module.exports = Test;
