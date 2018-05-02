90e6f2a15746d11eac26b8e426ff394e35e7b1fe
Allan James Lapid <ajLapid718@gmail.com>
2018-05-02 18:23:30 -0400

    finish student routes

--- routes/student.js
+++ routes/student.js
@@ -23,4 +23,33 @@ router.post("/student", function(req, res, next) {
   Student.create(req.body).then(savedStudent => res.json(savedStudent)).catch(err => console.log(err));
 });
 
+router.put("/student/:id", function(req, res, next) {
+  let targetId = req.params.id;
+  let infoToUpdate = req.body;
+
+  Student.update(infoToUpdate, {
+    where: {id: targetId},
+    returning: true,
+    plain: true
+  }).spread((numRows, affectedRows) => {
+    res.status(201).send(affectedRows);
+  });
+
+  // Find by student id
+  // Use instance method update throw in the req.body
+  // res send 201 and yeah;
+
+});
+
+router.delete("/student/:id", function(req, res, next) {
+  let targetId = req.params.id;
+
+  Student.destroy({
+    where: {
+      id: targetId
+  }
+}).then(destroyedStudent => res.status(204).send("Success"));
+
+})
+
 module.exports = router;
