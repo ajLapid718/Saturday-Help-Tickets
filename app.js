99dbc8f5c4b21192821b1d496bba15ae9f41490d
Allan James Lapid <ajLapid718@gmail.com>
2018-05-02 18:55:42 -0400

    one more spec

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 app.use("/", Student);
+app.use("/", Test);
 
 
 app.use(function(err, req, res, next) {
--- routes/test.js
+++ routes/test.js
@@ -2,4 +2,38 @@ const router = require('express').Router();
 const Test = require('../db/models/tests');
 const Student = require('../db/models/students');
 
+
+router.get("/test", function(req, res, next) {
+  Test.findAll().then(tests => res.json(tests));
+});
+
+router.get("/test/passing", function(req, res, next) {
+  Test.passing().then(tests => res.send(tests));
+});
+
+router.get("/test/:id", function(req, res, next) {
+  let targetId = req.params.id;
+  Test.findById(targetId).then(test => res.json(test));
+});
+
+router.get("/test/subject/:subject", function(req, res, next) {
+  Test.findBySubject(req.params.subject).then(tests => res.json(tests));
+});
+
+// router.post("/test/student/:studentId", function(req, res, next) {
+//   let targetId = req.params.studentId;
+//   let newInfo = req.body;
+//
+//   Test.create(newInfo).then(updatedStudent => res.status(201).json("Created"));
+// })
+
+router.delete("/test/:id", function(req, res, next) {
+  Test.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(destroyedTest => res.status(204).json(destroyedTest));
+});
+
+
 module.exports = router;
