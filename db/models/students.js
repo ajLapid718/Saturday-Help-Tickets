e29c467e45626b98238b251c19553bfe9deb1419
Allan James Lapid <ajLapid718@gmail.com>
2018-05-02 15:45:22 -0400

    set up student columns and getter virtual

--- db/models/students.js
+++ db/models/students.js
@@ -3,6 +3,33 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Student = db.define();
+const Student = db.define('student', {
+
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  },
+
+  fullName: {
+    type: Sequelize.VIRTUAL,
+    get: function() {
+      return this.getDataValue('firstName') + ' ' + this.getDataValue('lastName')
+    }
+  }
+
+});
 
 module.exports = Student;
